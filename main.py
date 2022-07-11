1c2508263e6ff0456faa762df0137c48b046dcd2
mukeshinchan <mukeshkannanduraisamy@gmail.com>
2022-07-11 10:48:43 +0530

    hvhn

--- main.py
+++ main.py
@@ -134,9 +134,9 @@ with butter.container():
         st.plotly_chart(fig6)
         
         
-data=df[df['year']==year_value]
-out2= data.groupby(['Month','Month_num'], as_index=False, sort=False).agg({'Sales':'sum'}).sort_values(by='Month_num')
-fig7=px.bar(out2,x='Month',y='Sales',color='Month',width=1200,height=750,text='Sales')
+data=df[(df['year']==year_value)]
+out2= data.groupby(['Month','Month_num','Region'], as_index=False, sort=False).agg({'Sales':'sum'}).sort_values(by='Month_num')
+fig7=px.bar(out2,x='Month',y='Sales',color='Region',width=1200,height=750,text='Sales')
 fig7.update_layout(xaxis=dict(showgrid=False),yaxis=dict(showgrid=False))
 fig7.update_layout({'plot_bgcolor':'rgba(0,0,0,0)','paper_bgcolor':'rgba(0,0,0,0)'})
 st.plotly_chart(fig7)   
